2593a15e1bf6025e549667302e9c0142dfe1ceb9
Манукянц Бабкен <b.manukyants@gmail.com>
2022-07-27 11:06:06 +0300

    выполнено

--- src/Main.java
+++ src/Main.java
@@ -11,22 +11,34 @@ public class Main {
             arrayMonth[i] = random.nextInt(100_000) + 100_000;
         }
         System.out.println(Arrays.toString(arrayMonth));
-        int sum = Arrays.stream(arrayMonth).sum();
+        int sum = 0;
+        for (int i = 0; i < arrayMonth.length; i++) {
+            sum = sum + arrayMonth[i];
+        }
         System.out.println ("Сумма трат за месяц составила " + sum + " рублей.");
+
+
+
+
         int maxSummInMonth = -1;
-        for (int i : arrayMonth) {
-            if (i > maxSummInMonth) {
-                maxSummInMonth = i;
+        int minSummInMonth = sum + 1;
+        for (int comparisonMonth : arrayMonth){
+            if (comparisonMonth < minSummInMonth){
+                minSummInMonth = comparisonMonth;
             }
+            if (comparisonMonth > maxSummInMonth){
+                maxSummInMonth = comparisonMonth;
+            }
+
         }
         System.out.println("Максимальная сумма трат за день составила " + maxSummInMonth + " рублей.");
-        int minSummInMonth = sum + 1;
-        for (int i : arrayMonth) {
-            if (i < minSummInMonth) {
-                minSummInMonth = i;
-            }
-        }
-        System.out.println("Минимальная сумма трат за день составила " + minSummInMonth + " рубл6ей.");
+        System.out.println("Минимальная сумма трат за день составила " + minSummInMonth + " рублей.");
+
+
+
+
+
+
         float averageSumInMonth = sum / arrayMonth.length;
         System.out.println("Средняя сумма трат за месяц составила " + averageSumInMonth + " рублей.");
         char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
